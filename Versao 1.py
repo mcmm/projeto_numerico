c8908e6afa6b23ec56650d992fba3b9431a0b2be
mcmm <mariacarlade@gmail.com>
2016-05-20 00:48:36 -0300

    Menu funcionando

--- Versao 1.py
+++ Versao 1.py
@@ -64,40 +64,70 @@ def romb(a, b, n, eps, itmax):
 
 ################ Programa Principal
 sair=0
-while sair !=1:
+while sair==0:
     escolha= int(input("Escolha uma das funções a seguir para calcular a integral:\n\
     1. x^4ln(x+sqrt(x^2+1))\n\
     2. 1/(1-x)\n\
     3. 1/pi(cos(sen(x))\n\
     4. sqrt(x)cos(x)\n\
-    5. sqrt()")
+    5. sqrt()\n\
+    6. Sair\n"))
+
+    if escolha==6:
+        break
+
+    print("Defina os intervalos da integração:\n")
+    a = float(input("Digite o valor de a:\n"))
+    b = float(input("Digite o valor de b:\n"))
+
+    mudar_erro = int(input("A precisão é de 10^(-6), deseja mudá-la?\n0. Não\n1. Sim\n"))
+    if mudar_erro==0:
+        eps = 0.000001
+    else:
+        eps=float(input("Digite o valor da precisão:\n"))
+
+    mudar_n = int(input("O número de colunas padrão é 4, deseja mudá-lo?\n0. Não\n1. Sim\n"))
+    if mudar_n==0:
+        n = 4
+    else:
+        n=int(input("Digite o valor de n:\n"))
+
+    mudar_itmax = int(input("O número máximo de iterações é 20, deseja mudá-lo?\n0. Não\n1. Sim\n"))
+    if mudar_itmax==0:
+        itmax = 20
+    else:
+        itmax=float(input("Digite o valor máximo de iterações:\n"))
 
     if escolha ==1:
         def funcao1 (x):
-            f1 = (x**4)*log((x+sqrt(x^2+1),e)
-            return f1 
+            f1 = (x**4)*log(x+sqrt(x**2+1),e)
+            return f1
     elif escolha ==2:
         def funcao1 (x):
             f1 = 1/(1-x)
-            return f1 
+            return f1
     elif escolha ==3:
         def funcao1 (x):
-            f1 = cos(sen(x))
-            return f1 
+            f1 = cos(sin(x))
+            return f1
     elif escolha ==4:
         def funcao1 (x):
             f1 = sqrt(x)*cos(x)
-            return f1 
+            return f1
     elif escolha ==5:
         def funcao1 (x):
-            f1 = x
-            return f1 
-    print("Defina os intervalos da integração:\n")
-    a = int(input("Digite o valor de a:"))
-    b = int(input("Digite o valor de b:"))
-                            
-    integral = romb(0, pi/2, 4, 0.000001, 10)
-print(integral)
+            f1 = 4*a
+            return f1
+    if escolha==3:
+        integral =((1/pi)* romb(a, b, n, eps, itmax))
+    else:
+        integral = romb(a, b, n, eps, itmax)
+
+    print("O valor da integral para a precisão escolhida é de: ", integral)
+
+
+
+
                 
             
         
