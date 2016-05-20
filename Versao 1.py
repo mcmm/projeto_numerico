8208655502a14002998e20a48fda41f0b16997c0
bruunocb <bdacostabraga@yahoo.com.br>
2016-05-20 20:07:34 -0300

    Versão FINAAAAAAAL

--- Versao 1.py
+++ Versao 1.py
@@ -46,10 +46,11 @@ def romb(a, b, n, eps, itmax):
 
         
     while (iteracoes <= itmax):
+
         iteracoes += 1
         if (abs(T[n][n]-T[n][n-1]) <= eps*T[n][n]):
-            print("O método convergiu pela diferença. Foram necessárias {} iterações".format(iteracoes-1))
-            print("Precisão:", abs(T[n][n]-T[n][n-1]))
+            print("\n   O método convergiu pela diferença. Foram necessárias {} iterações".format(iteracoes-1))
+            print("   Precisão:", abs(T[n][n]-T[n][n-1]))
             break
         else:
             for i in range(0, n):
@@ -66,78 +67,152 @@ def romb(a, b, n, eps, itmax):
                     T[n][k] = Tik  
     integral = T[n][n]
     if iteracoes==itmax:
-        print("O método atingiu o número máximo de iterações")
-    
+        print("\n   O método atingiu o número máximo de iterações")
 
     return integral
 
 ################
 
 ################ Programa Principal
-sair=0
+
+sair = 0
+eps = 0.000001
+n = 4
+itmax = 20 
+
 while sair==0:
-    escolha= int(input("\nEscolha uma das funções a seguir para calcular a integral:\n\
-    1. x^4ln(x+sqrt(x^2+1))\n\
-    2. 1/(1-x)\n\
-    3. 1/pi(cos(sen(x))\n\
-    4. sqrt(x)cos(x)\n\
-    5. Perímetro da Elipse de eixo de comprimento 2 e 3\n\
-    6. Sair\n"))
 
-    if escolha==6:
-        break
-
-    print("Defina os intervalos da integração:")
-    a = float(input("Digite o valor de a:\n"))
-    b = float(input("Digite o valor de b:\n"))
-
-    mudar_erro = int(input("A precisão é de 10^(-6), deseja mudá-la?\n0. Não\n1. Sim\n"))
-    if mudar_erro==0:
-        eps = 0.000001
-    else:
-        eps=float(input("Digite o valor da precisão:\n"))
-
-    mudar_n = int(input("O número de colunas padrão é 4, deseja mudá-lo?\n0. Não\n1. Sim\n"))
-    if mudar_n==0:
-        n = 4
-    else:
-        n=int(input("Digite o valor de n:\n"))
-
-    mudar_itmax = int(input("O número máximo de iterações é 20, deseja mudá-lo?\n0. Não\n1. Sim\n"))
-    if mudar_itmax==0:
-        itmax = 20
-    else:
-        itmax=float(input("Digite o valor máximo de iterações:\n"))
-
+    print("\n\nAs variáveis padrões atuais são: n =",n,", Epslon =",eps,", ITMAX =",itmax)
+    print("\n   1. Alterar as variáveis padrões (n, epslon, ITMAX)")
+    print("   2. Integrar x^4ln(x+sqrt(x^2+1))")
+    print("   3. Integrar 1/(1-x)")
+    print("   4. Integrar 1/pi(cos(sen(x))")
+    print("   5. Integrar sqrt(x)cos(x)")
+    print("   6. Perímetro da Elipse de eixos de comprimento 2 e 3")
+    print("   7. Sair")
+    escolha = int(input("\n   Escolha uma das opções acima:"))          
+                     
+       
     if escolha ==1:
+        print("\n   Escolha qual variável você deseja alterar:")
+        print("   1. Alterar 'n'")
+        print("   2. Alterar 'Epslon'")
+        print("   3. Alterar 'ITMAX'\n")
+        mudar = int(input())
+
+        if mudar == 1:
+            n=int(input("Digite o valor de n:\n"))
+        elif mudar == 2:
+            eps=float(input("Digite o valor da precisão:\n"))
+        elif mudar == 3:
+            itmax=float(input("Digite o valor máximo de iterações:\n"))
+
+    elif escolha ==2:
         def funcao1 (x):
             f1 = (x**4)*log(x+sqrt(x**2+1),e)
             return f1
-    elif escolha ==2:
+        print("\n   Escolha uma das opções:")
+        print("   1. Integrar usando o intervalo padrão: a = 0, b = 2")
+        print("   2. Integrar usando outro intervalo")
+        escolha2 = int(input())
+        if escolha2 == 1:
+            a = 0
+            b = 2
+        else:
+            print("\n   Defina os intervalos da integração:")
+            a = float(input("   Digite o valor de a:"))
+            b = float(input("   Digite o valor de b:"))
+        integral = romb(a, b, n, eps, itmax)
+        print("   O valor da integral para a precisão escolhida é de: ", integral)
+
+    elif escolha ==3:
         def funcao1 (x):
             f1 = 1/(1-x)
             return f1
-    elif escolha ==3:
+        print("\n   Escolha uma das opções:")
+        print("   1. Integrar usando o intervalo padrão: a = 0, b = 0.995")
+        print("   2. Integrar usando outro intervalo")
+        escolha2 = int(input())
+        if escolha2 == 1:
+            a = 0
+            b = 0.995
+        else:
+            print("\n   Defina os intervalos da integração:")
+            a = float(input("   Digite o valor de a:"))
+            b = float(input("   Digite o valor de b:"))
+        integral = romb(a, b, n, eps, itmax)
+        print("   O valor da integral para a precisão escolhida é de: ", integral)
+
+    elif escolha ==4:
         def funcao1 (x):
             f1 = cos(sin(x))
             return f1
-    elif escolha ==4:
-        def funcao1 (x):
-            f1 = sqrt(x)*cos(x)
-            return f1
-    elif escolha ==5:
-        def funcao1 (x):
-            f1 = sqrt(((1.5*cos(x))**2)+(sin(x)**2))
-            return f1
-    if escolha==3:
+        print("\n   Escolha uma das opções:")
+        print("   1. Integrar usando o intervalo padrão: a = 0, b = pi")
+        print("   2. Integrar usando outro intervalo")
+        escolha2 = int(input())
+        if escolha2 == 1:
+            a = 0
+            b = pi
+        else:
+            print("\n   Defina os intervalos da integração:")
+            a = float(input("   Digite o valor de a:"))
+            b = float(input("   Digite o valor de b:"))
         integral =((1/pi)* romb(a, b, n, eps, itmax))
-        print("O valor da integral para a precisão escolhida é de: ", integral)
-    else:
+        print("   O valor da integral para a precisão escolhida é de: ", integral)
+
+    elif escolha ==5:
+        print("\n   Escolha uma das opções:")
+        print("   1. Integrar usando a função padrão")
+        print("   2. Integrar usando a função após a troca de variável x=y^2")
+        escolha2 = int(input())
+        if escolha2 == 1:
+            def funcao1 (x):
+                f1 = sqrt(x)*cos(x)
+                return f1
+            print("\n   Escolha uma das opções:")
+            print("   1. Integrar usando o intervalo padrão: a = 0, b = pi/2")
+            print("   2. Integrar usando outro intervalo")
+            escolha3 = int(input())
+            if escolha3 == 1:
+                a = 0
+                b = pi/2
+            else:
+                print("\n   Defina os intervalos da integração:")
+                a = float(input("   Digite o valor de a:"))
+                b = float(input("   Digite o valor de b:"))
+            
+        else:
+            def funcao1 (x):
+                f1 = 2*(x**2)*cos((x**2)) ##função após a mudança de variável x=y^2
+                return f1
+            print("\n   Escolha uma das opções:")
+            print("   1. Integrar usando o intervalo padrão: a = 0, b = sqrt(pi/2)")
+            print("   2. Integrar usando outro intervalo")
+            escolha2 = int(input())
+            if escolha3 == 1:
+                a = 0
+                b = sqrt(pi/2)
+            else:
+                print("\n   Defina os intervalos da integração:")
+                a = float(input("   Digite o valor de a:"))
+                b = float(input("   Digite o valor de b:"))
         integral = romb(a, b, n, eps, itmax)
-        print("O valor da integral para a precisão escolhida é de: ", integral)
+        print("   O valor da integral para a precisão escolhida é de: ", integral)
 
+    elif escolha ==6:
+        def funcao1 (x):            
+            f1 = sqrt(((1.5*cos(x))**2)+(sin(x)**2))            
+            return f1
+        a=0
+        b=2*pi
+        integral = romb(a, b, n, eps, itmax)
+        print("   O Perímetro da Elipse em questão é: ", integral)
+  
+    elif escolha==7:
+        break
 
-
+    
 
 
 
