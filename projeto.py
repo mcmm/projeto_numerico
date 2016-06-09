7a847e4de29364464234ee433f11f074ba3d0cf4
mcmm <mariacarlade@gmail.com>
2016-06-09 18:13:13 -0300

    ultimas modificações

--- projeto.py
+++ projeto.py
@@ -14,13 +14,26 @@ from funcoes_projeto import*
 
 ############ Programa Principal
 
+
+escolha_n = int(input("Escolha um dos ns abaixo, ou então digite\n1. 7\n2. 15\n3. 31\n4. 63\n5. Outro\n"))
+if escolha_n==1:
+    n = 7
+elif escolha_n==2:
+    n = 15
+elif escolha_n==3:
+    n = 31
+elif escolha_n==4:
+    n = 63
+elif escolha_n==5:
+    n = int(input("Digite o valor de n:"))
+
 # Definição de constantes
 eps = 0.000001
-n = int(input("Digite o valor de n:"))
 h = 1/(n+1)
 x_i = []
-#y_i = []
+y_i = []
 u_barra=[]
+erro_maximo = 0
 d = [] # coluna com o lado direito do sistema
 phi_linha_primeiro_intervalo = 1/h #Primeiro intervalo: [Xi-1, X]
 phi_linha_segundo_intervalo = (-1)/h #Segundo intervalo: [X, Xi+1]
@@ -43,8 +56,8 @@ for i in range(1,n+1):
     
     
 #Montagem da matriz A
-escolha = int(input("O que você deseja fazer em relação ao material?\n1. Usar material constante (k=1)\n2. Usar material variável(k variável)\n"))
-if escolha==1:
+escolha_k = int(input("O que você deseja fazer em relação ao material?\n1. Usar material constante (k=1)\n2. Usar material variável(k variável)\n"))
+if escolha_k==1:
     k=1
     a = criar_diagonal_inferior(n, (-1)/h)#diagonal inferior
     b = criar_vetor(n, 2/h) #diagonal principal
@@ -59,23 +72,15 @@ if escolha==1:
     for contador in range(0, n):
         print("alpha[{}] = {}".format(contador, alphas[contador]))
 
-elif escolha==2:
+elif escolha_k==2:
     k=2
-
-#Definindo o vetor y_i
-#for i in range(1,n+1):
-#    y=i/(10*n)
-#    y_i.append(y)
-##for contador in range(0, n):
-##    print("y[{}] = {}".format(contador, y_i[contador]))
-for contador in range(0, n):
-    print("x[{}] = {}".format(contador, x_i[contador]))
+    ##implementar aqui o k variavel
 
 #Calculando u_barra
-for i in range(0, 10*n+1): #para o y
+for i in range(10*n+1): #para o y
     elemento = 0
     y=i/(10*n)
-    print("y = {}".format(y))
+    y_i.append(y)
     for j in range(1,n+1): #para o alpha
         if (y>=x_i[j-1]) and (y<x_i[j]):
             phi = lambda x:(((x-x_i[j-1])/h))
@@ -85,7 +90,12 @@ for i in range(0, 10*n+1): #para o y
             elemento += alphas[j-1]*phi(y)
     u_barra.append(elemento)
 
-for contador in range(0, 10*n+1):
-    print("u_barra[{}] = {}".format(contador, u_barra[contador]))
+#Teste de convergencia
+u = lambda x: (x**2)*((x-1)**2)
+for i in range(10*n+1):
+    if abs(u_barra[i]-u(y_i[i]))>erro_maximo:
+        erro_maximo = u_barra[i]-u(y_i[i])
+print("Erro:", erro_maximo)
+
 
 
